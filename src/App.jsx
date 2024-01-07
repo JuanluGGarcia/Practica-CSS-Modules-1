20a060a89c5e2f3c1c19d24deb27b5c65c44c1c4
Juan Luis Garcia Garcia <juanluisggarcia@gmail.com>
2024-01-07 13:21:08 +0100

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,10 @@ const App = () => {
 			<section className="data__container">
 				<p className="summary">Summary</p>
 				
-				<TestCard style="red" path="./assets/images/icon-reaction.svg" test="Reaction" punctuation={80}/>
-				<TestCard style="yellow" path="./assets/images/icon-memory.svg" test="Memory" punctuation={80}/>
-				<TestCard style="green" path="./assets/images/icon-verbal.svg" test="Verbal" punctuation={61}/>
-				<TestCard style="blue" path="./assets/images/icon-visual.svg" test="Visual" punctuation={73}/>
+				<TestCard color="red" bgColor="bg__red" path="./assets/images/icon-reaction.svg" test="Reaction" punctuation={85}/>
+				<TestCard color="yellow" bgColor="bg__yellow" path="./assets/images/icon-memory.svg" test="Memory" punctuation={80}/>
+				<TestCard color="green" bgColor="bg__green" path="./assets/images/icon-verbal.svg" test="Verbal" punctuation={61}/>
+				<TestCard color="blue" bgColor="bg__blue" path="./assets/images/icon-visual.svg" test="Visual" punctuation={73}/>
 				
 				<button className="button">Continue</button>
 					{/* <Data /> */}
--- src/components/testCard/TestCard.jsx
+++ src/components/testCard/TestCard.jsx
@@ -1,15 +1,19 @@
 import style from './testCard.module.css'
 // import styles from './testCard.module.css'
 
-const TestCard = ({ path, test, punctuation }) => {
+const TestCard = (props) => {
+    
     return (
-        <div className={style.testing__Container}>
-            <div className={`${style.test} ${style.bg__red}`}>
-                <img className={style.test__image} src={path} alt="icon" />
-                <p className={`${style.test} ${style.red}`}>{test}</p>              
+        // <div className={style.testing__Container}>
+            
+            <div className={`${style.testing__Container} ${style[props.bgColor]}`}>
+                <div className={style.test}>
+                    <img className={style.test__image} src={props.path} alt="icon" />
+                    <p className={`${style.test} ${style[props.color]}`}>{props.test}</p>              
+                </div>
+                <p>{props.punctuation}<span className={style.test__punctuation}> / 100</span></p>
             </div>
-            <p>{punctuation}<span className={style.test__punctuation}> / 100</span></p>
-        </div>
+        // </div>
     );
 };
 export default TestCard;
